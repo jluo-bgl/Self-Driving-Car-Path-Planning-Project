bf2c7b199f16f8c294966f0d5056c2f9df0667bb
James Luo <jluo@bglcorp.com.au>
2018-08-13 15:30:27 +1000

    add change lane path planing strategy

--- src/main.cpp
+++ src/main.cpp
@@ -3,11 +3,13 @@
 #include <uWS/uWS.h>
 #include <chrono>
 #include <iostream>
+#include <array>
 #include <thread>
 #include <vector>
 #include "Eigen-3.3/Eigen/Core"
 #include "Eigen-3.3/Eigen/QR"
 #include "json.hpp"
+#include "spline.h"
 
 using namespace std;
 
@@ -173,7 +175,40 @@ void testAllBeenSetupAndCarShouldGoStraight(double car_x, double car_y, double c
 }
 
 
-class CarStatus {
+struct XY{
+  double x;
+  double y;
+
+  XY() {
+    this->x = 0.0;
+    this->y = 0.0;
+  }
+
+  XY(double x, double y) {
+    this->x = x;
+    this->y = y;
+  }
+};
+
+struct Lane {
+  int laneNumberFromMiddleOfRoad;
+  int d;
+
+  Lane(int laneNumberFromMiddleOfRoad) {
+    this->laneNumberFromMiddleOfRoad = laneNumberFromMiddleOfRoad;
+    this->d = 2 + 4 * laneNumberFromMiddleOfRoad;
+  }
+
+  bool inSameLane(int dOfOtherCar) {
+    if ((dOfOtherCar < (this->d + 2)) && (dOfOtherCar > (this->d - 2))) {
+      return true;
+    }else{
+      return false;
+    }
+  }
+};
+
+class DriveEnvironment {
  public:
   double car_x;
   double car_y;
@@ -183,18 +218,20 @@ class CarStatus {
   double car_speed;
 
   // Previous path data given to the Planner
-//  auto previous_path_x;
-//  auto previous_path_y;
+  json previous_path_x;
+  json previous_path_y;
   // Previous path's end s and d values
   double end_path_s;
   double end_path_d;
 
   // Sensor Fusion Data, a list of all other cars on the same side of the road.
-//  auto sensor_fusion;
+  json sensor_fusion;
+
+  int prev_size;
 
 
-  CarStatus(double car_x, double car_y, double car_s, double car_d, double car_yaw, double car_speed,
-                double end_path_s, double end_path_d) {
+  DriveEnvironment(double car_x, double car_y, double car_s, double car_d, double car_yaw, double car_speed,
+                double end_path_s, double end_path_d, json previous_path_x, json previous_path_y, json sensor_fusion) {
     this->car_x = car_x;
     this->car_y = car_y;
     this->car_s = car_s;
@@ -203,7 +240,86 @@ class CarStatus {
     this->car_speed = car_speed;
     this->end_path_s = end_path_s;
     this->end_path_d = end_path_d;
+
+    this->previous_path_x = previous_path_x;
+    this->previous_path_y = previous_path_y;
+    this->sensor_fusion = sensor_fusion;
+
+    this->prev_size = previous_path_x.size();
   }
+
+  bool hasEnoughPreviewPath() {
+    return this->prev_size > 2;
+  }
+
+  std::tuple<XY, XY> last2Point() {
+    if (this->hasEnoughPreviewPath()) {
+      return this->last2PointBasedPreviousPath();
+    }else{
+      return this->last2PointBasedCurrentCarPosition();
+    }
+  };
+
+  std::tuple<XY, double> referancePoints() {
+    if (this->hasEnoughPreviewPath()) {
+      double ref_x = this->previous_path_x[this->prev_size - 1];
+      double ref_y = this->previous_path_y[this->prev_size - 1];
+      double ref_x_prev = this->previous_path_x[this->prev_size - 2];
+      double ref_y_prev = this->previous_path_y[this->prev_size - 2];
+
+      double ref_yaw = atan2(ref_y - ref_y_prev, ref_x - ref_x_prev);
+
+      return std::make_tuple(XY(ref_x, ref_y), ref_yaw);
+    }else{
+      XY xy = XY(this->car_x, this->car_y);
+      double ref_yaw = deg2rad(this->car_yaw);
+      return std::make_tuple(xy, ref_yaw);
+    }
+  };
+
+  bool tooCloseToFrontCar(Lane lane) {
+    bool too_close = false;
+
+    for (int i = 0; i < this->sensor_fusion.size(); i++) {
+      //car is in my lane
+      float d = this->sensor_fusion[i][6];
+      if (lane.inSameLane(d)) {
+        double vx = this->sensor_fusion[i][3];
+        double vy = this->sensor_fusion[i][4];
+        double check_speed = sqrt(vx * vx + vy * vy);
+        double check_car_s = this->sensor_fusion[i][5];
+
+        check_car_s += ((double)this->prev_size * 0.02 * check_speed);
+        if ((check_car_s > this->car_s) && (check_car_s - this->car_s) < 30) {
+          too_close = true;
+        }
+      }
+    }
+    return too_close;
+  }
+
+ protected:
+  std::tuple<XY, XY> last2PointBasedCurrentCarPosition() {
+    double prev_car_x = this->car_x - cos(this->car_yaw);
+    double prev_car_y = this->car_y - sin(this->car_yaw);
+    return std::make_tuple(
+        XY(prev_car_x, prev_car_y),
+        XY(this->car_x, this->car_y));
+  };
+
+  std::tuple<XY, XY> last2PointBasedPreviousPath() {
+    double ref_x = this->previous_path_x[this->prev_size - 1];
+    double ref_y = this->previous_path_y[this->prev_size - 1];
+
+    double ref_x_prev = this->previous_path_x[this->prev_size - 2];
+    double ref_y_prev = this->previous_path_y[this->prev_size - 2];
+
+    return std::make_tuple(
+        XY(ref_x_prev, ref_y_prev),
+        XY(ref_x, ref_y));
+  };
+
+
 };
 
 
@@ -229,20 +345,7 @@ class PathPlanningStrategy{
 
   virtual ~PathPlanningStrategy() {};
 
-
-  void followTheLine(CarStatus &drivingStatus, vector<double> &next_x_vals, vector<double> &next_y_vals) {
-    double dist_inc = 0.4;
-    for(int i = 0; i < 50; i++)
-    {
-      double next_s = drivingStatus.car_s + (i + 1) * dist_inc;
-      double next_d = 6;
-      vector<double> xy = getXY(next_s, next_d, this->map_waypoints_s, this->map_waypoints_x, this->map_waypoints_y);
-      next_x_vals.push_back(xy[0]);
-      next_y_vals.push_back(xy[1]);
-    }
-  }
-
-  void planningPath(CarStatus &drivingStatus, vector<double> &next_x_vals, vector<double> &next_y_vals) {
+  virtual void planningPath(DriveEnvironment &drivingStatus, vector<double> &next_x_vals, vector<double> &next_y_vals) {
     for(int i = 0; i < 50; i++)
     {
       double nextx;
@@ -254,7 +357,19 @@ class PathPlanningStrategy{
   }
 
  protected:
-  virtual std::tuple<double, double> nextXY(CarStatus &drivingStatus, int currentStep) = 0;
+  virtual std::tuple<double, double> nextXY(DriveEnvironment &drivingStatus, int currentStep) = 0;
+
+  XY getXYInternal(double s, double d) {
+    vector<double> xy = getXY(s, d, this->map_waypoints_s, this->map_waypoints_x, this->map_waypoints_y);
+    return XY(xy[0], xy[1]);
+  }
+
+  void addToXYVector(vector<XY> points, vector<double> &ptsx, vector<double> &ptsy) {
+    for(XY xy : points) {
+      ptsx.push_back(xy.x);
+      ptsy.push_back(xy.y);
+    }
+  }
 };
 
 
@@ -263,15 +378,19 @@ class PathPlanningFollowLineStrategy: public PathPlanningStrategy {
  public:
   PathPlanningFollowLineStrategy(vector<double> map_waypoints_x,
   vector<double> map_waypoints_y, vector<double> map_waypoints_s,
-  vector<double> map_waypoints_dx, vector<double> map_waypoints_dy): PathPlanningStrategy(
-      map_waypoints_x, map_waypoints_y, map_waypoints_s, map_waypoints_dx, map_waypoints_dy) {}
+  vector<double> map_waypoints_dx, vector<double> map_waypoints_dy, Lane lane): PathPlanningStrategy(
+      map_waypoints_x, map_waypoints_y, map_waypoints_s, map_waypoints_dx, map_waypoints_dy
+  ) {
+    this->lane = lane;
+  }
 
  protected:
   double dist_inc = 0.4;
+  Lane lane = Lane(0);
 
-  std::tuple<double, double> nextXY(CarStatus &drivingStatus, int currentStep) {
+  std::tuple<double, double> nextXY(DriveEnvironment &drivingStatus, int currentStep) {
     double next_s = drivingStatus.car_s + (currentStep + 1) * dist_inc;
-    double next_d = 6;
+    double next_d = this->lane.d;
     vector<double> xy = getXY(next_s, next_d, this->map_waypoints_s, this->map_waypoints_x, this->map_waypoints_y);
     return std::make_tuple(xy[0], xy[1]);
   }
@@ -287,12 +406,197 @@ class PathPlanningGoStraightStrategy: public PathPlanningStrategy {
 
  protected:
   double dist_inc = 0.4;
-  std::tuple<double, double> nextXY(CarStatus &drivingStatus, int currentStep) {
+  std::tuple<double, double> nextXY(DriveEnvironment &drivingStatus, int currentStep) {
     return std::make_tuple(
         drivingStatus.car_x+(dist_inc*currentStep)*cos(deg2rad(drivingStatus.car_yaw)),
         drivingStatus.car_y+(dist_inc*currentStep)*sin(deg2rad(drivingStatus.car_yaw))
     );
   }
+};
+
+class PathPlanningSlowDownWhenApprochingCar: public PathPlanningStrategy {
+ public:
+  PathPlanningSlowDownWhenApprochingCar(vector<double> map_waypoints_x,
+                                 vector<double> map_waypoints_y, vector<double> map_waypoints_s,
+                                 vector<double> map_waypoints_dx, vector<double> map_waypoints_dy): PathPlanningStrategy(
+      map_waypoints_x, map_waypoints_y, map_waypoints_s, map_waypoints_dx, map_waypoints_dy) {}
+
+ public:
+  void planningPath(DriveEnvironment &drivingStatus, vector<double> &next_x_vals, vector<double> &next_y_vals) {
+    vector<double> ptsx;
+    vector<double> ptsy;
+
+    if (drivingStatus.prev_size > 0) {
+      drivingStatus.car_s = drivingStatus.end_path_s; // changed the status!!!!
+    }
+
+    bool too_close = drivingStatus.tooCloseToFrontCar(lane);
+
+    if(too_close) {
+      ref_velocity -= 0.224;
+    }else if (ref_velocity < 49.5) {
+      ref_velocity += 0.244;
+    }
+
+    XY point1;
+    XY point2;
+    std::tie(point1, point2) = drivingStatus.last2Point();
+
+    int d = lane.d;
+    XY next_wp0 = this->getXYInternal(drivingStatus.car_s + 30, d);
+    XY next_wp1 = this->getXYInternal(drivingStatus.car_s + 60, d);
+    XY next_wp2 = this->getXYInternal(drivingStatus.car_s + 90, d);
+
+    this->addToXYVector({point1, point2, next_wp0, next_wp1, next_wp2}, ptsx, ptsy);
+
+    XY refXY;
+    double ref_yaw;
+    std::tie(refXY, ref_yaw) = drivingStatus.referancePoints();
+    double ref_x = refXY.x;
+    double ref_y = refXY.y;
+
+    for (int i = 0; i < ptsx.size(); i++) {
+      double shift_x = ptsx[i] - ref_x;
+      double shift_y = ptsy[i] - ref_y;
+      ptsx[i] = (shift_x * cos(0 - ref_yaw) - shift_y * sin(0 - ref_yaw));
+      ptsy[i] = (shift_x * sin(0 - ref_yaw) + shift_y * cos(0 - ref_yaw));
+    }
+
+    tk::spline splineInterpolator;
+    splineInterpolator.set_points(ptsx, ptsy);
+
+    for (int i = 0; i < drivingStatus.previous_path_x.size(); i++) {
+      next_x_vals.push_back(drivingStatus.previous_path_x[i]);
+      next_y_vals.push_back(drivingStatus.previous_path_y[i]);
+    }
+
+    double target_x = 30.0;
+    double target_y = splineInterpolator(target_x);
+    double target_dist = sqrt((target_x*target_x)+(target_y*target_y));
+
+    double x_add_on = 0;
+    for (int i = 1; i <= 50 - drivingStatus.previous_path_x.size(); i++) {
+      double N = (target_dist / (0.02 * this->ref_velocity/2.24));
+      double x_point = x_add_on + target_x/N;
+      double y_point = splineInterpolator(x_point);
+
+      x_add_on = x_point;
+
+      double x_ref = x_point;
+      double y_ref = y_point;
+
+      x_point = (x_ref * cos(ref_yaw) - y_ref * sin(ref_yaw));
+      y_point = (x_ref * sin(ref_yaw) + y_ref * cos(ref_yaw));
+
+      x_point += ref_x;
+      y_point += ref_y;
+
+      next_x_vals.push_back(x_point);
+      next_y_vals.push_back(y_point);
+    }
+  }
+
+ protected:
+  Lane lane = Lane(1);
+  double ref_velocity = 0.0; //mph
+
+  std::tuple<double, double> nextXY(DriveEnvironment &drivingStatus, int currentStep) {
+    return std::make_tuple(0.0, 0.0);
+  };
+};
+
+class PathPlanningChangeLaneWhenApprochingCar: public PathPlanningStrategy {
+ public:
+  PathPlanningChangeLaneWhenApprochingCar(vector<double> map_waypoints_x,
+                                        vector<double> map_waypoints_y, vector<double> map_waypoints_s,
+                                        vector<double> map_waypoints_dx, vector<double> map_waypoints_dy): PathPlanningStrategy(
+      map_waypoints_x, map_waypoints_y, map_waypoints_s, map_waypoints_dx, map_waypoints_dy) {}
+
+ public:
+  void planningPath(DriveEnvironment &drivingStatus, vector<double> &next_x_vals, vector<double> &next_y_vals) {
+    vector<double> ptsx;
+    vector<double> ptsy;
+
+    if (drivingStatus.prev_size > 0) {
+      drivingStatus.car_s = drivingStatus.end_path_s; // changed the status!!!!
+    }
+
+    bool too_close = drivingStatus.tooCloseToFrontCar(lane);
+
+    if(too_close) {
+      ref_velocity -= 0.224;
+      if (lane.laneNumberFromMiddleOfRoad > 0) {
+        this->lane = Lane(0);
+      }
+    }else if (ref_velocity < 49.5) {
+      ref_velocity += 0.244;
+    }
+
+    XY point1;
+    XY point2;
+    std::tie(point1, point2) = drivingStatus.last2Point();
+
+    int d = lane.d;
+    XY next_wp0 = this->getXYInternal(drivingStatus.car_s + 30, d);
+    XY next_wp1 = this->getXYInternal(drivingStatus.car_s + 60, d);
+    XY next_wp2 = this->getXYInternal(drivingStatus.car_s + 90, d);
+
+    this->addToXYVector({point1, point2, next_wp0, next_wp1, next_wp2}, ptsx, ptsy);
+
+    XY refXY;
+    double ref_yaw;
+    std::tie(refXY, ref_yaw) = drivingStatus.referancePoints();
+    double ref_x = refXY.x;
+    double ref_y = refXY.y;
+
+    for (int i = 0; i < ptsx.size(); i++) {
+      double shift_x = ptsx[i] - ref_x;
+      double shift_y = ptsy[i] - ref_y;
+      ptsx[i] = (shift_x * cos(0 - ref_yaw) - shift_y * sin(0 - ref_yaw));
+      ptsy[i] = (shift_x * sin(0 - ref_yaw) + shift_y * cos(0 - ref_yaw));
+    }
+
+    tk::spline splineInterpolator;
+    splineInterpolator.set_points(ptsx, ptsy);
+
+    for (int i = 0; i < drivingStatus.previous_path_x.size(); i++) {
+      next_x_vals.push_back(drivingStatus.previous_path_x[i]);
+      next_y_vals.push_back(drivingStatus.previous_path_y[i]);
+    }
+
+    double target_x = 30.0;
+    double target_y = splineInterpolator(target_x);
+    double target_dist = sqrt((target_x*target_x)+(target_y*target_y));
+
+    double x_add_on = 0;
+    for (int i = 1; i <= 50 - drivingStatus.previous_path_x.size(); i++) {
+      double N = (target_dist / (0.02 * this->ref_velocity/2.24));
+      double x_point = x_add_on + target_x/N;
+      double y_point = splineInterpolator(x_point);
+
+      x_add_on = x_point;
+
+      double x_ref = x_point;
+      double y_ref = y_point;
+
+      x_point = (x_ref * cos(ref_yaw) - y_ref * sin(ref_yaw));
+      y_point = (x_ref * sin(ref_yaw) + y_ref * cos(ref_yaw));
+
+      x_point += ref_x;
+      y_point += ref_y;
+
+      next_x_vals.push_back(x_point);
+      next_y_vals.push_back(y_point);
+    }
+  }
+
+ protected:
+  Lane lane = Lane(1);
+  double ref_velocity = 0.0; //mph
+
+  std::tuple<double, double> nextXY(DriveEnvironment &drivingStatus, int currentStep) {
+    return std::make_tuple(0.0, 0.0);
+  };
 };
 
 
@@ -333,10 +637,12 @@ int main() {
   	map_waypoints_dy.push_back(d_y);
   }
 
-//  PathPlanningStrategy *followLineStrategy = new PathPlanningGoStraightStrategy(map_waypoints_x, map_waypoints_y, map_waypoints_s, map_waypoints_dx, map_waypoints_dy);
-  PathPlanningStrategy *followLineStrategy = new PathPlanningFollowLineStrategy(map_waypoints_x, map_waypoints_y, map_waypoints_s, map_waypoints_dx, map_waypoints_dy);
+  PathPlanningStrategy *pathPlanningStrategy = new PathPlanningChangeLaneWhenApprochingCar(map_waypoints_x, map_waypoints_y, map_waypoints_s, map_waypoints_dx, map_waypoints_dy);
+//  PathPlanningStrategy *pathPlanningStrategy = new PathPlanningSlowDownWhenApprochingCar(map_waypoints_x, map_waypoints_y, map_waypoints_s, map_waypoints_dx, map_waypoints_dy);
+//  PathPlanningStrategy *pathPlanningStrategy = new PathPlanningGoStraightStrategy(map_waypoints_x, map_waypoints_y, map_waypoints_s, map_waypoints_dx, map_waypoints_dy);
+//  PathPlanningStrategy *pathPlanningStrategy = new PathPlanningFollowLineStrategy(map_waypoints_x, map_waypoints_y, map_waypoints_s, map_waypoints_dx, map_waypoints_dy, Lane(1));
 
-  h.onMessage([&map_waypoints_x,&map_waypoints_y,&map_waypoints_s,&map_waypoints_dx,&map_waypoints_dy, &followLineStrategy](uWS::WebSocket<uWS::SERVER> ws, char *data, size_t length,
+  h.onMessage([&map_waypoints_x,&map_waypoints_y,&map_waypoints_s,&map_waypoints_dx,&map_waypoints_dy, &pathPlanningStrategy](uWS::WebSocket<uWS::SERVER> ws, char *data, size_t length,
                      uWS::OpCode opCode) {
     // "42" at the start of the message means there's a websocket message event.
     // The 4 signifies a websocket message
@@ -364,7 +670,7 @@ int main() {
           	double car_speed = j[1]["speed"];
 
           	// Previous path data given to the Planner
-          	auto previous_path_x = j[1]["previous_path_x"];
+            json previous_path_x = j[1]["previous_path_x"];
           	auto previous_path_y = j[1]["previous_path_y"];
           	// Previous path's end s and d values 
           	double end_path_s = j[1]["end_path_s"];
@@ -373,7 +679,10 @@ int main() {
           	// Sensor Fusion Data, a list of all other cars on the same side of the road.
           	auto sensor_fusion = j[1]["sensor_fusion"];
 
-			CarStatus drivingStatus = CarStatus(car_x, car_y, car_s, car_d, car_yaw, car_speed, end_path_s, end_path_d);
+			DriveEnvironment drivingStatus = DriveEnvironment(
+                car_x, car_y, car_s, car_d, car_yaw, car_speed, end_path_s, end_path_d,
+                previous_path_x, previous_path_y, sensor_fusion
+            );
 
           	json msgJson;
 
@@ -382,7 +691,7 @@ int main() {
 
             // Uncomment below, you car should go straight, otherwise your setup is not right
 			// testAllBeenSetupAndCarShouldGoStraight(car_x, car_y, car_yaw, next_x_vals, next_y_vals);
-            followLineStrategy->planningPath(drivingStatus, next_x_vals, next_y_vals);
+            pathPlanningStrategy->planningPath(drivingStatus, next_x_vals, next_y_vals);
 
           	// TODO: define a path made up of (x,y) points that the car will visit sequentially every .02 seconds
           	msgJson["next_x"] = next_x_vals;
@@ -435,5 +744,5 @@ int main() {
   }
   h.run();
 
-  delete followLineStrategy;
+  delete pathPlanningStrategy;
 }
--- tests/test2D.cpp
+++ tests/test2D.cpp
@@ -1,20 +1,6 @@
 #include "gtest/gtest.h"
-#include "../src/EntryPoint.h"
 
 TEST(KalmanFilter, ableToComplieTest) {
-  int resutl1 = entryPoint2D();
+  int resutl1 = 1;
   EXPECT_EQ(resutl1, 1);
-};
-
-TEST(KalmanFilter, twoDimentionalProcessFromFileTest) {
-  int resutl1 = processFile("/Users/james/git/Self-Driving-Car-Extended-Kalman-Filter-2D/tests/obj_pose-laser-radar-synthetic-input.txt");
-  EXPECT_EQ(resutl1, 0);
-};
-
-TEST(KalmanFilter, jacobianMatrixTest) {
-  jacobianMatrix();
-};
-
-TEST(KalmanFilter, rmseTest) {
-  calcRMSE();
 };
